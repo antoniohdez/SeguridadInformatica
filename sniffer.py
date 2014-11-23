165b22aae09fbaa578156c68d316f6dec81f4c4e
Antonio <antonio.hdez93@gmail.com>
2014-11-23 12:12:34 -0600

    Sniffer mejorado, se imprimen los datos del header y la información del mismo en hex.

--- sniffer.py
+++ sniffer.py
@@ -1,11 +1,54 @@
-import socket
-# the public network interface
-host = socket.gethostbyname(socket.gethostname())
-# create a raw socket and bind it to the public interface
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
-s.bind((host, 0))
-# Include IP headers
-s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-print s.recvfrom(65565)
-s.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+import socket, sys
+from struct import *
+
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+except socket.error , msg:
+	print msg
+	sys.exit()
+
+while True:
+	packet = s.recvfrom(65535)
+	packet = packet[0]
+	ip_header = packet[0:20] # first 20 characters
+	iph = unpack('!BBHHHBBH4s4s' , ip_header)
+	
+	version_ihl = iph[0]
+	version = version_ihl >> 4
+	ihl = version_ihl & 0xF
+	iph_length = ihl * 4
+	ttl = iph[5]
+	protocol = iph[6]
+	s_addr = socket.inet_ntoa(iph[8]);
+	d_addr = socket.inet_ntoa(iph[9]);
+	
+	print 'Version : ' + str(version)
+	print 'IP Header Length : ' + str(ihl)
+	print 'TTL : ' + str(ttl)
+	print 'Protocol : ' + str(protocol)
+	print ' Source Address : ' + str(s_addr)
+	print 'Destination Address : ' + str(d_addr)
+	
+	tcp_header = packet[iph_length:iph_length+20]
+	tcph = unpack('!HHLLBBHHH' , tcp_header)
+	
+	source_port = tcph[0]
+	dest_port = tcph[1]
+	sequence = tcph[2]
+	acknowledgement = tcph[3]
+	doff_reserved = tcph[4]
+	tcph_length = doff_reserved >> 4
+	
+	print 'Source Port : ' + str(source_port)
+	print 'Dest Port : ' + str(dest_port)
+	print 'Sequence Number : ' + str(sequence) 
+	print 'Acknowledgement : ' + str(acknowledgement)
+	print ' TCP header length : ' + str(tcph_length)
+	
+	h_size = iph_length + tcph_length * 4
+	data_size = len(packet) - h_size
+	
+	data = packet[h_size:] #data
+	
+	print repr(data)
+	print "\n\n\n"
